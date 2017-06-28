80310db28da514e059eb50474dc4db8148bc7181
Philip Putnam <putnamp989@gmail.com>
2017-06-27 21:26:40 -0700

    add ejs view engine to server.js, index.ejs rendering src > index.js

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ import config from './config';
 import express from 'express';
 
 const server = express();
+server.set('view engine', 'ejs');
 
 server.get('/', (req, res) => {
-  res.send('hello world!');
+  res.render('index');
 });
+
+server.use(express.static('public'));
 
 server.listen(config.port, () => {
   console.info('Express listening on port ', config.port);
