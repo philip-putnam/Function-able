5e27fdaaa89681045a0e213605b83ae7d0d30178
Philip Putnam <putnamp989@gmail.com>
2017-06-28 15:30:13 -0700

    add components directory to src directory, add App.js export to index.js for render

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <h1>Hello from index.js!</h1>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './components/App';
+
 ReactDOM.render(
-  <h1>Hello from index.js!</h1>,
+  <App />,
   document.getElementById('root')
 );
