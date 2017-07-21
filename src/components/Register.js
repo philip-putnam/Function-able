a005a2b283be0621ffa2a7b04eeda382a5fcff75
Philip Putnam <putnamp989@gmail.com>
2017-07-21 14:48:52 -0700

    add onSubmit & onRegister to Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
 
 export default class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: '' };
+  }
+
+  onRegister = e => {
+    console.log('onRegister', e.target.value);
+    this.setState({ name: e.target.value}); 
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+  }
+
   render() {
+    console.log(this.state.name);
     return (
-      <form>
-        <input type='text' />
+      <form onSubmit={this.onSubmit}>
+        <input type='text' onChange={this.onRegister} />
         <input type='submit' value='Submit' />
       </form>
     );
