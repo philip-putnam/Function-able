58878b960dfab9d54938b87a99e5d2988171b9d3
Philip Putnam <putnamp989@gmail.com>
2017-07-26 10:34:03 -0700

    add basic form for creating event

--- src/components/CreateFunction.js
+++ src/components/CreateFunction.js
@@ -2,7 +2,20 @@ import React from 'react';
 
 const CreateFunction = () => {
   return (
-    <h1>Create a new Function</h1>
+    <div>
+      <h1>Create a new Function</h1>
+      <form>
+        <label>Function/Event Name: </label>
+        <input type='text' /><br/>
+        <label>Function Date: </label>
+        <input type='text' /><br/>
+        <label>Funding Deadline: </label>
+        <input type='text' /><br/>
+        <label>Is this function dependent on meeting budget goals? </label>
+        <input type='text' /><br/>
+        <input type='submit' value='Submit' />
+      </form>
+    </div>
   );
 };
 
