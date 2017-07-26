69891fb691d164b3914dc86d8c76fb37a29d76a5
Philip Putnam <putnamp989@gmail.com>
2017-07-26 11:01:50 -0700

    modify Events.js to class importing Component from react

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-const Events = () => {
-  return (
-    <div>
-      <h1>Events</h1>
-      <Link to='/create-a-function'>Create New Function</Link>
-    </div>
-  );
-};
-
-export default Events;
+export default class Events extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Events</h1>
+        <Link to='/create-a-function'>Create New Function</Link>
+      </div>
+    );
+  }
+}
