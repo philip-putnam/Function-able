761f3d4d462a83feea917a82c84dab59a61cd832
Philip Putnam <putnamp989@gmail.com>
2017-07-26 13:15:09 -0700

    display state in Events.js using map()

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import CreateFunction from './CreateFunction';
+
 export default class Events extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { events: [{
+      name: 'hi', key: '1' },
+    {name: 'bye', key: '2'}] };
+  }
+
   render() {
     return (
       <div>
         <h1>Events</h1>
+        <h2>Your Functions & Events:</h2>
+        <ul>
+          {this.state.events.map((event) => {
+            return (
+              <li key={event.key}>{event.name}</li>
+            );
+          })}
+        </ul>
         <Link to='/create-a-function'>Create New Function</Link>
+        <CreateFunction events={this.state.events} />
       </div>
     );
   }
