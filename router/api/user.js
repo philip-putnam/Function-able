9790bea54970fb79024a5afe178f1734300c44e9
Philip Putnam <putnamp989@gmail.com>
2017-07-27 10:47:47 -0700

    post register user to mongo db

--- router/api/user.js
+++ router/api/user.js
@@ -3,4 +3,44 @@ const router = express.Router();
 
 const User = require('../../models/user');
 
+router.get('/register', (req, res) => {
+  return res.send('user created!');
+});
+
+router.post('/register', (req, res, next) => {
+  if (req.body.email &&
+  req.body.firstName &&
+  req.body.lastName &&
+  req.body.username &&
+  req.body.password &&
+  req.body.confirmPassword) {
+
+    if (req.body.password !== req.body.confirmPassword) {
+      let err = new Error('Passwords do not match');
+      err.status = 400;
+      return next;
+    }
+
+    const userData = {
+      email: req.body.email,
+      username: req.body.username,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      password: req.body.password
+    };
+
+    User.create(userData, function(error, user) {
+      if(error) {
+        return next(error);
+      } else {
+        return res.redirect('/');
+      }
+    });
+  } else {
+    let err = new Error('All fields required');
+    err.status = 400;
+    return next(err);
+  }
+});
+
 module.exports = router;
