268bf8fdfde8598b9584a80c3c4f39545c560ff8
Philip Putnam <putnamp989@gmail.com>
2017-08-05 14:59:58 -0700

    refactor index.js and app.js, create TemplateContainer.js, hookup Store and Provider from redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Header from './Header';
@@ -9,24 +9,19 @@ import Event from './Event';
 import Login from './Login';
 import CreateFunction from './CreateFunction';
 
-export default class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <Header />
-          <div>
-            <div className='container'>
-              <Route exact path='/' component={Home} />
-              <Route path='/registration' component={Register} />
-              <Route path='/events' component={Events} />
-              <Route path='/event/:name' component={Event} />
-              <Route path='/login' component={Login} />
-              <Route path='/create-a-function' component={CreateFunction} />
-            </div>
-          </div>
-        </div>
-      </BrowserRouter>
-    );
-  }
+export default function App(props) {
+  return (
+    <BrowserRouter>
+      <div className='container'>
+        <Header />
+        <p>{props.progress}</p>
+        <Route exact path='/' component={Home} />
+        <Route path='/registration' component={Register} />
+        <Route path='/events' component={Events} />
+        <Route path='/event/:name' component={Event} />
+        <Route path='/login' component={Login} />
+        <Route path='/create-a-function' component={CreateFunction} />
+      </div>
+    </BrowserRouter>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
+
+import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 
-import App from './components/App';
+import Store from './store';
+import TemplateContainer from './components/TemplateContainer';
 require('style-loader!../public/css/styles.css');
 
-render(
-  <div>
-    <App />
-  </div>,
-  document.querySelector('#react-app')
-);
+const renderApp = (Component) => {
+  render (
+    <div>
+      <Provider store={Store}>
+        <Component />
+      </Provider>
+    </div>,
+    document.querySelector('#react-app')
+  );
+};
+
+renderApp(TemplateContainer);
+
+
+// render(
+//   <div>
+//     <App />
+//   </div>,
+//   document.querySelector('#react-app')
+// );
